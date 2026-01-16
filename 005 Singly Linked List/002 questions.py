dba640c94ef13a2cd9b73ea4b0771fb63437b521
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-16 18:44:20 +0530

    Add solution for Add Two Numbers problem
    Implemented the addTwoNumbers method in the Solution class to add two numbers represented by linked lists. The function handles digit-wise addition and carry, returning the sum as a new linked list.

--- 005 Singly Linked List/002 questions.py
+++ 005 Singly Linked List/002 questions.py
@@ -61,3 +61,49 @@ class Solution(object):
             
             if c>1:
                 return None
+
+# 2. Add Two Numbers
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, head1: Optional[ListNode], head2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1=head1
+        curr2=head2
+        
+        ans=ListNode(-1)
+        c=0
+        curr3=ans
+        
+        while curr1!=None or curr2!=None:
+            total=c
+            c=0
+            
+            if curr1!=None:
+                total+=curr1.val
+                curr1=curr1.next
+            if curr2!=None:
+                total+=curr2.val
+                curr2=curr2.next
+            
+            if total>9:
+                c=1
+                total-=10
+            
+            newNode=ListNode(total)
+            curr3.next=newNode
+            curr3=curr3.next
+            
+        if c>0:
+            newNode=ListNode(c)
+            curr3.next=newNode
+            
+        return ans.next
