f0c1de1cbb5e7b183f844879efe24ce652a936df
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-25 11:17:11 +0530

    Add solution for Next Greater Element I problem
    Implemented the nextGreaterElement method for LeetCode problem 496. The function uses a stack to efficiently find the next greater element for each number in nums2 and maps results for nums1.

--- 008 Stack/questions.py
+++ 008 Stack/questions.py
@@ -99,3 +99,22 @@ class Solution:
             return ''.join(result)
         
         return build(s) == build(t)
+    
+
+# 496. Next Greater Element I
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        next_greater = {}
+        stack = []
+        
+        for num in nums2:
+            while stack and stack[-1] < num:
+                smaller_num = stack.pop()
+                next_greater[smaller_num] = num
+            stack.append(num)
+        
+        # For elements that have no next greater element
+        for num in stack:
+            next_greater[num] = -1
+        
+        return [next_greater[num] for num in nums1]
