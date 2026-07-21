828f2b7a3e0ab6b6ae3eb97c78c7b3b6bb6198bc
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-21 22:25:44 +0530

    3 Ways to Access Base Class

---
+++ 001 OOP/007 baseClass.py
@@ -0,0 +1,20 @@
+class chai:
+    def __init__(self, type_ , strength):
+        self.type_ = type_
+        self.strength = strength
+
+# class gingerchai(chai):
+#     def __init__(self, type_, strength, spice_level):
+#         self.type_ = type_
+#         self.strength = strength
+#         self.spice_level = spice_level
+
+# class gingerchai(chai):
+#     def __init__(self, type_, strength, spice_level):
+#         chai.__init__(self, type_, strength)
+#         self.spice_level = spice_level
+
+class gingerchai(chai):
+    def __init__(self, type_, strength, spice_level):
+        super().__init__(type_, strength)
+        self.spice_level = spice_level
